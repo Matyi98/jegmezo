5cd9b40ce1f4bf10b128938dae0339d437eb4ffa
tieger <tieger@sch.bme.hu>
2020-04-25 21:19:41 +0200

    teszt2

--- test/scene/Map0Tests.java
+++ test/scene/Map0Tests.java
@@ -38,20 +38,20 @@ public class Map0Tests {
     }
 
     @Before
-    public void initialise() throws UnsupportedEncodingException {
+    public void initialise() {
         addCommand("tst loadmap map0.txt");
+    }
+
+    @Test
+    public void AltalanosFunkciok() throws UnsupportedEncodingException {
         addCommand("p turn d");
         addCommand("p move");
         addCommand("s map");
         addCommand("p move");
         addCommand("s map");
         addCommand("s stats");
-
         setInputString();
-    }
 
-    @Test
-    public void AltalanosFunkciok() throws UnsupportedEncodingException {
         Main.NewGame(InStream);
         Scanner scanner = new Scanner(baos.toString());
         System.out.println(baos.toString());
@@ -67,16 +67,39 @@ public class Map0Tests {
 
     @Test
     public void MezoBeszakadasa() throws UnsupportedEncodingException {
+        addCommand("p turn d");
+        addCommand("p turn d");
+        addCommand("p move");
+        addCommand("p turn d");
+        addCommand("p turn d");
+        addCommand("p move");
+        addCommand("p skip");
+        addCommand("p skip");
+        addCommand("p turn d");
+        addCommand("p turn d");
+        addCommand("p move");
+        addCommand("s map");
+        setInputString();
+
         Main.NewGame(InStream);
         Scanner scanner = new Scanner(baos.toString());
         System.out.println(baos.toString());
 
         assertEquals("Successful turn: 5",scanner.nextLine());
-        assertEquals("Successful move: 5",scanner.nextLine());
-        assertEquals("U20p0; S000M; H0000; H0000; S00cE; U3000; H0000; S00e0; S00s0; H0000; H0000; U200K; S000K;",scanner.nextLine());
-        assertEquals("Successful move: 2",scanner.nextLine());
-        assertEquals("U20p0; S000ME; H0000; H0000; S00c0; U3000; H0000; S00e0; S00s0; H0000; H0000; U200K; S000K",scanner.nextLine());
-        assertEquals("HP: 5",scanner.nextLine());
-        assertEquals("AP: 2",scanner.nextLine());
+        assertEquals("Successful turn: 10",scanner.nextLine());
+        assertEquals("Successful move: 10",scanner.nextLine());
+        assertEquals("Successful turn: 9",scanner.nextLine());
+        assertEquals("Successful turn: 12",scanner.nextLine());
+        assertEquals("Successful move: 12",scanner.nextLine());
+        assertEquals("Skip",scanner.nextLine());
+        assertEquals("Skip",scanner.nextLine());
+
+        assertEquals("Successful turn: 10",scanner.nextLine());
+        assertEquals("Successful turn: 12",scanner.nextLine());
+        assertEquals("Successful move: 12",scanner.nextLine());
+        assertEquals("U20p0; S000M; H0000; H0000; S00c0; U3000; H0000; S00e0; S00s0; H0000; H0000; U200KEK; S0000;",scanner.nextLine());
+        assertEquals("Game Over",scanner.nextLine());
+
+
     }
 }
