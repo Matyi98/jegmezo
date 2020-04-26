40435d7003ccf98ef63054b43bd872a2c8f4d1ab
Alex Seewald <alexrad89@gmail.com>
2020-04-26 14:11:59 +0200

    FieldBeahviour package.

--- src/fields/behaviours/FieldBehaviourWithTent.java
+++ src/fields/behaviours/FieldBehaviourWithTent.java
@@ -6,23 +6,64 @@ import scene.GameController;
 
 import java.util.Collection;
 
+/**
+ * A Fieldnek az tenttel rendelkező állapotának viselkedését implementáló osztály.
+ */
 public class FieldBehaviourWithTent extends FieldBehaviour{
     public FieldBehaviourWithTent(Field f) {
         super(f);
         GameController.OutStream.println("Successful TentBuild");
     }
 
+    /**
+     * Mivel a FieldBehaviourWithTent olyan állapotot reprezentál, amely
+     * megvédi a havazástól a FieldBehaviourWithTentet tulajdonoló Fielden
+     * álló Entityket, ezér nem hajt vére a paraméterként átvett Entityken
+     * sebzést.
+     * @param entities A FieldBehaviourWithIgloot tulajdonoló Fielden
+     *                 álló Entityk.
+     */
+    @Override
+    public void performSnow(Collection<Entity> entities) {
+
+    }
+
+    /**
+     * Mivel a FieldBehaviourWithTentet tulajdonoló Field FieldBehaviour attribútuma már
+     * FieldBehaviourWithTent, amire váltást a metódus szolgálna, nem kell azt átváltania.
+     * @return False értékkel tér vissza mindig.
+     */
+    @Override
+    public boolean buildTent() {
+        return false;
+    }
+
+    /**
+     * Átváltja a FieldBehaviourWithTentet tulajdonoló Field, FieldBehaviour attribútumát
+     * StandardFieldBehaviourre, ezzel azt szimulálva, mintha lerombolódna Fieldről a tent.
+     */
     @Override
     public void destroyTent(){
         field.setBehaviour(new StandardFieldBehaviour(field));
     }
 
+    /**
+     * Átváltja a Field, FieldBehaviour attribútumát FieldBehaviourWithIgloora,
+     * ezzel iglooval rendelkező viselkedést injektálva a Fieldbe.
+     * @return Amennyiben átváltotta a StandardFieldBehaviour tulajdonoló Field
+     * FieldBehaviour attribútumát úgy true, ellenező esetben false értékkel tér vissza.
+     */
     @Override
     public boolean buildIgloo() {
         field.setBehaviour(new FieldBehaviourWithIgloo(field));
         return true;
     }
 
+    /**
+     * Összeütközteti a FieldBehaviourt túlajdonoló Field Entityjeit a Fieldre lépő Entityvel.
+     * @param enteringEntity A FieldBehaviourWithTent túlajdonoló Fieldre lépő Entity.
+     * @param standingEntities A FieldBehaviourWithTent túlajdonoló Fielden lévő Entityk.
+     */
     @Override
     public void collideEntities(Entity enteringEntity, Collection<Entity> standingEntities) {
         for(Entity e : standingEntities){
@@ -31,7 +72,7 @@ public class FieldBehaviourWithTent extends FieldBehaviour{
     }
 
     /**
-     * Kiírja az állapot röviden.
+     * Kiírja az állapotot röviden.
      */
     @Override
     public void ShowShort() {
