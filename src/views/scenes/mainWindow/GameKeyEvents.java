7fc02d4ba1f911d1c5447e7291aa4015781ad74d
gcsaba99 <gergely.csaba99@gmail.com>
2020-05-16 14:49:58 +0200

    Keyeventek kezelése, parser a szám inputokra

--- src/views/scenes/mainWindow/GameKeyEvents.java
+++ src/views/scenes/mainWindow/GameKeyEvents.java
@@ -11,7 +11,14 @@ public class GameKeyEvents implements EventHandler<KeyEvent> {
 
     @Override
     public void handle(KeyEvent keyEvent) {
-        switch (keyEvent.getCharacter().toUpperCase()) {
+        String input = keyEvent.getCharacter().toUpperCase();
+
+        if(isItemIndex(input) != -1) {
+            GameController.GetInstance().Execute("p use " + isItemIndex(input));
+            System.out.println("p use " + isItemIndex(input));
+        }
+
+        switch (input) {
             case "D":
                 GameController.GetInstance().Execute("p turn D");
                 break;
@@ -21,6 +28,36 @@ public class GameKeyEvents implements EventHandler<KeyEvent> {
             case "W":
                 GameController.GetInstance().Execute("p move");
                 break;
+            case "E": // End turn (skip)
+                GameController.GetInstance().Execute("p skip");
+                break;
+            case "Q":
+                GameController.GetInstance().Execute("p special");
+                break;
+            case "S":
+                GameController.GetInstance().Execute("p shovel");
+                break;
+            case "P":
+                GameController.GetInstance().Execute("p pickup");
+                break;
+        }
+    }
+
+    /**
+     * Eldönti egy kapott szövegről hogy az egy megfelelő item index,
+     * vagyis egy 1 és 6 között lévő egész szám.
+     * @param str a kapott szöveg
+     * @return ha megfelelő item index akkor maga az index, egyébként pedig -1
+     */
+    private int isItemIndex(String str) {
+        try {
+            int itemIndex = Integer.parseInt(str);
+            if (itemIndex >= 1 && itemIndex <= 6)
+                return itemIndex;
+            else
+                return -1;
+        } catch(NumberFormatException e){
+            return -1;
         }
     }
 }
