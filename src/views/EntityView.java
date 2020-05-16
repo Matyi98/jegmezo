4cc4717038cea6c6cbce9aa36ab0b60ecff44c34
NandorRefi <NandorRefi@DESKTOP-56DAIOA>
2020-05-16 21:59:00 +0200

    Entity-k megjelenítése és aktív játékos megjelölése

--- src/views/EntityView.java
+++ src/views/EntityView.java
@@ -1,16 +1,51 @@
 package views;
 
 import entities.Entity;
+import game.GameController;
+import javafx.scene.image.ImageView;
 
 public class EntityView extends ViewBase {
     private Entity data;
+    private final int dimensions = 50;
+
     public EntityView(Entity e) {
         super(e.GetTexturePath());
         this.data = e;
+
+        setMaxHeight(dimensions);
+
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(dimensions);
+        imageView.setFitWidth(dimensions);
+        imageView.setPreserveRatio(true);
+
+        getChildren().add(imageView);
+
+        Update();
     }
 
     @Override
     public void Update() {
-        //TODO: Implement this.
+        if(GameController.GetInstance().GetActivePlayer() == data){
+            enableBorder();
+        }
+        else{
+            disableBorder();
+        }
     }
+
+    private void disableBorder(){
+        setStyle( "-fx-border-style: solid inside;" +
+                "-fx-border-width: 0;" +
+                "-fx-border-radius: 10;" +
+                "-fx-border-color: green;");
+    }
+
+    private void enableBorder(){
+        setStyle("-fx-border-style: solid inside;"+
+                "-fx-border-width: 4;" +
+                "-fx-border-radius: 10;" +
+                "-fx-border-color: green;");
+    }
+
 }
