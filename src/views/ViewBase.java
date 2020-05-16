570a85c6a10bfeb4e9ae125803c6ace081e75acc
Alex Seewald <alexrad89@gmail.com>
2020-05-16 18:09:44 +0200

    image beolvasás - kész? vagy mégse?!?!...

--- src/views/ViewBase.java
+++ src/views/ViewBase.java
@@ -2,14 +2,30 @@ package views;
 
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
+import javafx.scene.image.ImageView;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public abstract class ViewBase extends StackPane implements IView {
     protected Image image;
 
     public ViewBase(String imagePath) {
-        if (!imagePath.equals(""))
-            this.image = new Image(imagePath);
-        else
-            this.image = null;
+
+
+        Image img = null;
+        try(InputStream is = Files.newInputStream(Paths.get(imagePath))){
+            img = new Image(is);
+        } catch (IOException e) {}
+
+        ImageView imgView = new ImageView(img);
+
+//        if (!imagePath.equals(""))
+//            this.image = new Image(imagePath);
+//        else
+//            this.image = null;
+
+        getChildren().add(imgView);
     }
 }
