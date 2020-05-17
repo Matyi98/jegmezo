c03407282fe73651f3b50c624e7180e8b51e83f3
NandorRefi <NandorRefi@DESKTOP-56DAIOA>
2020-05-17 16:40:48 +0200

    Mostmár elfogadhatóan néz ki a GameScene osztály

--- src/views/scenes/mainWindow/GameScene.java
+++ src/views/scenes/mainWindow/GameScene.java
@@ -1,56 +1,65 @@
 package views.scenes.mainWindow;
-
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import game.GameController;
 import views.BoardView;
+import views.IUpdatable;
 import views.InfoPanelView;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class GameScene extends Scene {
     private static final double WIDTH = 1280;
     private static final double HEIGHT = 720;
 
     private Pane root = new Pane();
-    private static BoardView boardView;
-    private static InfoPanelView infoPanelView;
+
+    private static ArrayList<IUpdatable> views = new ArrayList<>();
 
     public GameScene(File file) {
         super(new StackPane(), WIDTH, HEIGHT);
-        boardView = new BoardView(GameController.GetInstance().GetBoard(), file);
-        infoPanelView = new InfoPanelView();
-        initialize();
+        BoardView boardView = new BoardView(GameController.GetInstance().GetBoard(), file);
+        InfoPanelView infoPanelView = new InfoPanelView();
+
+        setBackground();
+
+        initBoardView(boardView);
+        initInfoPanelView(infoPanelView);
+
+        views.clear();
+        views.add(boardView);
+        views.add(infoPanelView);
+
+        setOnKeyTyped(new GameKeyEvents());
+
         setRoot(root);
     }
 
-    private void initialize() {
-        root.setBackground(
-                new Background(
-                        new BackgroundFill(
-                                Color.rgb(40, 40, 60),
-                                CornerRadii.EMPTY,
-                                Insets.EMPTY
-                        )));
-        root.getChildren().add(boardView);
-        root.getChildren().add(infoPanelView);
+    private void setBackground(){
+        root.setStyle("-fx-background-color: rgb(40, 40, 60)");
+    }
 
+    private void initBoardView(BoardView boardView){
         boardView.setLayoutX(10);
         boardView.setLayoutY(10);
         boardView.setPrefSize(HEIGHT-20+100, HEIGHT-20);
 
+        root.getChildren().add(boardView);
+    }
+
+    private void initInfoPanelView(InfoPanelView infoPanelView){
         infoPanelView.setLayoutX(HEIGHT+100);
         infoPanelView.setLayoutY(10);
         infoPanelView.setPrefSize(WIDTH - 10 - HEIGHT-100,HEIGHT-20);
 
-        setOnKeyTyped(new GameKeyEvents());
+        root.getChildren().add(infoPanelView);
     }
 
     public static void UpdateAllViews(){
-        boardView.Update();
-        infoPanelView.Update();
+        for(IUpdatable view: views){
+            view.Update();
+        }
     }
 
 
